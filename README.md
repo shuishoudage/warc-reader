1c5d9db548ded4a9162160e8848360c7f5d29942
Taotao Pan <pantaotao@icloud.com>
2019-08-14 15:09:19 +1000

    warc-reader

---
+++ README.md
@@ -0,0 +1,40 @@
+## Environment
+
+-   OS: Mac OS Mojave 10.14
+-   docker: 18.09
+
+## Instructions
+
+After installed docker on your machine, send following command on machine
+
+```sh
+docker-compose up
+```
+
+Waiting a while to fetch and store documents, then you are ready to play with
+documents. Port 9200 is exposed as elasticsearch, you can start query by using
+`curl` or chrome extension webui [dejavu](https://chrome.google.com/webstore/detail/dejavu-elasticsearch-web/jopjeaiilkcibeohjdmejhoifenbnmlh?hl=en). Port 27017 is exposed as mongodb, you can access by `mongo` command line tool
+or ui based like [mongodb_compass](https://www.mongodb.com/products/compass).
+
+mongodb only store metadata. Whereas, elasticsearch only store body content, they
+can be cross referenced by id field.
+
+## Configuration
+
+There are two environment variables need to be configured. Check docker-compose.yml
+file. There is one environment variable under warc-reader-service section called
+`MAX_RECORD_FETCH` which can be modified based on following explanations.
+
+-   0: take no action, this is typically useful when you have fetched certain
+    number of docs, then you do not want to fetch more
+-   \>0: when value greater than 0 means the number of records you want to fetch
+-   <0: when value less than 0, it indicates clean up database and elasticsearch
+
+Another is the `DB_NAME` which is simply the name of database and the index name
+of elasticsearch.
+
+## Caveats
+
+set `MAX_RECORD_FETCH` in a small pace, so that even you forget to change this
+variable, everytime when you start the service it will only fetch a small amount
+of new records, otherwise it may easily blow up your storage.
---
+++ backend-warc-reader/warc-reader.py
@@ -0,0 +1,285 @@
+from elasticsearch import Elasticsearch
+from pymongo import errors
+from uuid import uuid4
+from warcio.archiveiterator import ArchiveIterator
+import elasticsearch
+import hashlib
+import json
+import logging
+import os
+import pymongo
+import re
+import requests
+import sys
+import threading
+import time
+
+
+# Mongodb metadata collection cursor
+mongodb = None
+
+# elastic connector
+es = None
+
+
+def _logger_level_setup():
+    """logger level setup
+    """
+    root = logging.getLogger()
+    root.setLevel(logging.ERROR)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+
+def _mongodb_connection_setup(dbName):
+    """database setup
+    """
+    try:
+        global mongodb
+        client = pymongo.MongoClient('mongodb://mongodb-service:27017')
+        mongodb = client[dbName]
+        logging.debug('Mongodb connected')
+    except errors.ConnectionFailure as error:
+        logging.error(error, exc_info=True)
+
+
+def _elastic_connection_setup(index):
+    """elasticsearch setup
+    """
+    global es
+    connected = False
+    try:
+        es = Elasticsearch(
+            hosts=[{'host': 'elasticsearch-service'}], max_retries=100)
+        es.ping()
+
+    except elasticsearch.ConnectionError as error:
+        logging.error(error, exc_info=True)
+    except elasticsearch.ConnectionTimeout as error:
+        logging.error(error, exc_info=True)
+    # this line of code handles the sync bugs of elasticsearch in docker
+    while not connected:
+        try:
+            es.cluster.health(wait_for_status="yellow")
+            connected = True
+            if connected:
+                mapping = '''
+                {
+                    "mappings":{
+                        "_doc":{
+                            "properties":{
+                                "id": {"type": "text"},
+                                "content": {"type": "text"}
+                            }
+                        }
+                    }
+                }
+                '''
+                es.indices.create(index=index, ignore=400, body=mapping)
+                logging.debug('Elasticsearch connected')
+        except ConnectionError as error:
+            time.sleep(0.1)
+            logging.error(error, exc_info=True)
+
+
+def _collection_count():
+    """this function is used for skip already fetched records
+
+    Returns:
+        {int} -- return the count of records, if the collection was cleaned up
+        return 0
+    """
+    try:
+        return mongodb.metadata.count()
+    except errors.CursorNotFound as error:
+        return 0
+
+
+def _insert_metadata(id, response_headers, warc_headers):
+    """convert list of tuples of response header infomation
+    to json object then store it into mongodb
+
+    Arguments:
+        response_headers {[tuple]} -- a list of key-value response metadata
+        warc_headers {[tuple]} -- a list of key-value warc metadata
+    """
+    # in case there is no response header
+    if len(response_headers) and len(warc_headers) != 0:
+        try:
+            headers = {'id': id, 'metadata': {'response': dict(
+                response_headers), 'warc': dict(warc_headers)}}
+            mongodb.metadata.insert_one(headers)
+        except errors.OperationFailure as error:
+            logging.error(error, exc_info=True)
+        except errors.ExecutionTimeout as error:
+            logging.error(error, exc_info=True)
+
+
+def _insert_body_content(index, id, stream):
+    """save the body raw content of warc file
+
+    Arguments:
+        stream {content_stream} -- the decompressed stream
+    """
+    try:
+        body = stream.read()
+        # find the encoding type
+        has_encoding = re.findall(b"""<meta.*?charset=(.*?)[>\s]""", body)
+        if has_encoding:  # when find encoding, use that encoding
+            body = body.decode(
+                has_encoding[0].decode('utf-8')
+                .lower().strip(';:\' "\/'), 'backslashreplace')
+        else:  # otherwise only use utf-8
+            body = body.decode('utf-8', 'backslashreplace')
+        serialized_body = json.dumps({'id': id, 'content': body})
+        res = es.index(index=index, body=serialized_body)
+
+    except errors.OperationFailure as error:
+        logging.error(error, exc_info=True)
+    except errors.ExecutionTimeout as error:
+        logging.error(error, exc_info=True)
+
+
+def _fetch_records(index, url, maxRecords=5):
+    """fetch the warc record by given url and maxRecords, when maxRecords
+    reaches the fetching loop will be broken
+
+    Arguments:
+        url {string} -- warc file url 
+
+    Keyword Arguments:
+        maxRecords {int} -- [the maximum records to fetch] (default: {5})
+    """
+
+    # {int} the number to skip fetch
+    stored_records = _collection_count()
+
+    resp = requests.get(url, stream=True)
+    for record in ArchiveIterator(resp.raw, arc2warc=True):
+        if record.rec_type == 'response':
+            if record.http_headers.get_header('Content-Type') == 'text/html':
+                if stored_records > 0:
+                    stored_records -= 1
+                    continue
+                # {string} the id for both metadata and content body
+                id = uuid4().hex
+                _insert_metadata(id, record.http_headers.headers,
+                                 record.rec_headers.headers)
+                _insert_body_content(index, id, record.content_stream())
+                maxRecords -= 1
+                if maxRecords == 0:
+                    break
+
+
+def _clean_up_database():
+    """clearn up mongdb collection
+    """
+    try:
+        mongodb.metadata.drop()
+        logging.debug('mongodb cleaned up')
+    except errors.InvalidOperation as error:
+        logging.error(error, exc_info=True)
+    except errors.OperationFailure as error:
+        logging.error(error, exc_info=True)
+
+
+def _clean_up_elastic(index='test'):
+    """clean up index for further testing
+
+    Keyword Arguments:
+        index {str} -- elasticsearch index (default: {'test'})
+    """
+    try:
+        es.indices.delete(index=index, ignore=[
+                          400, 404])
+        logging.debug('elasticsearch index cleaned up')
+    except elasticsearch.NotFoundError as error:
+        logging.error(error, exc_info=True)
+    except elasticsearch.RequestError as error:
+        logging.error(error, exc_info=True)
+
+
+def _clean_up(index):
+    """the combination of clean up methods
+    """
+    _clean_up_database()
+    _clean_up_elastic(index)
+
+
+def query_elastic(index='test'):
+    """sample elastic query
+
+    Keyword Arguments:
+        index {str} -- which index you want to search (default: {'test'})
+    """
+    try:
+        res = es.search(index=index,
+                        body={'query': {'match_all': {}}})
+        logging.info(res)
+    except elasticsearch.NotFoundError as error:
+        logging.error(error, exc_info=True)
+
+
+def setup_all_connections(dbName):
+    _logger_level_setup()
+    _mongodb_connection_setup(dbName)
+    _elastic_connection_setup(dbName)
+
+
+def fetch_store_or_cleanup(dbName, url, maxRecordsFetch):
+    """the main action of this project, there are three actions
+    to take based on maxRecordsFetch.
+    - 0: doing nothing
+    - >0: fetch the given number of records
+    - <0: cleanup database and elasticsearch
+
+    Arguments:
+        maxRecordsFetch {int} -- the max number of records to fetch each time
+    """
+    if maxRecordsFetch > 0:
+        logging.debug('Metadata collection task is running...')
+        _fetch_records(dbName, url, maxRecordsFetch)
+    # if maxRecordsFetch is negative, actioin of clean up
+    # database and elasticsearch will be taken
+    elif maxRecordsFetch < 0:
+        _clean_up(dbName)
+    else:
+        return
+
+
+def task_runner():
+    url = "https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2018-26/segments/1529267859766.6/warc/CC-MAIN-20180618105733-20180618125538-00027.warc.gz"
+    while True:
+        try:
+            # the envionment variable that passed from docker-compose file
+            maxRecordFetch = int(os.getenv('MAX_RECORD_FETCH', 100))
+            # dbName can be used both on mongodb database name and
+            # elasticsearch index name
+            dbName = os.getenv('DB_NAME', 'test')
+
+            # setup mongodb and elastic context environment
+            setup_all_connections(dbName)
+
+            # main task part, fetch warc files and store them into database
+            # and indexing in elastich search, or for experiment purpose
+            # these can be cleaned up
+            fetch_store_or_cleanup(dbName, url, maxRecordFetch)
+
+        except ValueError as e:
+            logging.error(e, exc_info=True)
+        except Exception as e:
+            logging.debug('Exception did not handled')
+            logging.error(e, exc_info=True)
+        finally:
+            return
+
+
+if __name__ == '__main__':
+    thread = threading.Thread(target=task_runner)
+    thread.start()
+    thread.join()
